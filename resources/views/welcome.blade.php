079806328af4b63f14b593f3452135e2d9d821e3
promisepioneer <fifirman000@gmail.com>
2024-08-14 09:40:11 +0700

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
 
-    {{ dd(DB::table('finger_log')->get()) }}
+    {{ dd(DB::table('finger_logs')->get()) }}
 
     <title>Laravel</title>
 
