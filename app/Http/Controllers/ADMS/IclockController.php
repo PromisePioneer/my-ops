e8739220d7236a60f151d23a3dacfbe9e0e2a18e
promisepioneer <fifirman000@gmail.com>
2024-08-15 12:17:14 +0700

    update

--- app/Http/Controllers/ADMS/IclockController.php
+++ app/Http/Controllers/ADMS/IclockController.php
@@ -98,7 +98,9 @@ class IclockController extends Controller
                     $q['status3'] = $this->validateAndFormatInteger($data[4] ?? null);
                     $q['status4'] = $this->validateAndFormatInteger($data[5] ?? null);
                     $q['status5'] = $this->validateAndFormatInteger($data[6] ?? null);
-                    Attendances::create($q);
+                    $test = Attendances::create($q);
+
+                    dd($test);
                     $tot++;
                     // dd(DB::getQueryLog());
                 }
