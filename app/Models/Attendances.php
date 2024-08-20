1030f5957a1d8f7cc896aed722c64428adc3e5a2
promisepioneer <fifirman000@gmail.com>
2024-08-20 12:22:49 +0700

    update

--- app/Models/Attendances.php
+++ app/Models/Attendances.php
@@ -91,8 +91,7 @@ class Attendances extends Model
 
                 // Calculate lateness in minutes
                 $actualCheckIn = Carbon::parse($checkIn->timestamp);
-                $minutesLate = $actualCheckIn->greaterThan($expectedCheckIn) ? $expectedCheckIn->diffInMinutes($actualCheckIn,
-                    false) : 0;
+                $minutesLate = $actualCheckIn->greaterThan($expectedCheckIn) ? $expectedCheckIn->diffInMinutes($actualCheckIn) : 0;
 
                 return [
                     'name' => $checkIn->user_name,
