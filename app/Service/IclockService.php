50f58de9f066345e4bfec7e59e760f75d55022bc
promisepioneer <fifirman000@gmail.com>
2024-08-20 11:09:31 +0700

    update

--- app/Service/IclockService.php
+++ app/Service/IclockService.php
@@ -214,11 +214,10 @@ class IclockService
 
     private function processCheckIn(array $attendanceData, $shift, string $date, string $time): void
     {
-        dd($this->isValidTime($time, $shift->time_to_checkin, $shift->end_time_to_checkin));
         if ($this->isValidTime($time, $shift->time_to_checkin, $shift->end_time_to_checkin)) {
             $existingRecord = $this->getAttendanceRecord($attendanceData['employee_id'], $date);
 
-
+            dd(!$existingRecord);
             if (!$existingRecord) {
                 Attendances::create($attendanceData);
             }
