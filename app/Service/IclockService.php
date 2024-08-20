6198234f57036839261268e4069f8804c264f481
promisepioneer <fifirman000@gmail.com>
2024-08-20 11:10:14 +0700

    update

--- app/Service/IclockService.php
+++ app/Service/IclockService.php
@@ -216,8 +216,6 @@ class IclockService
     {
         if ($this->isValidTime($time, $shift->time_to_checkin, $shift->end_time_to_checkin)) {
             $existingRecord = $this->getAttendanceRecord($attendanceData['employee_id'], $date);
-
-            dd(!$existingRecord);
             if (!$existingRecord) {
                 Attendances::create($attendanceData);
             }
