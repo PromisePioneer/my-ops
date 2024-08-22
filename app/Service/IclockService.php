588a24c6cedf83754f45ba6dd9d479459f5e3dd9
promisepioneer <fifirman000@gmail.com>
2024-08-22 20:01:26 +0700

    updating attendancesSUmmary

--- app/Service/IclockService.php
+++ app/Service/IclockService.php
@@ -240,7 +240,6 @@ class IclockService
         if ($this->isValidTime($time, $shift->time_to_checkout, $shift->end_time_to_checkout)) {
             $existingCheckOut = $this->getAttendanceRecord($attendanceData['employee_id'], $date, 'desc');
 
-            dd('haha');
 
             if (!$existingCheckOut || $existingCheckOut->status1 != 1) {
                 Attendances::create($attendanceData);
