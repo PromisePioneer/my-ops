f2c1df32251cd4d9fc4e76480280aa4c045b928e
promisepioneer <fifirman000@gmail.com>
2024-08-22 19:45:14 +0700

    updating attendancesSUmmary

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Attendances;
 use Illuminate\View\View;
 
 class HomeController extends Controller
@@ -21,6 +22,7 @@ class HomeController extends Controller
      */
     public function index(): View
     {
+        dd(Attendances::all());
         return view('home');
     }
 }
