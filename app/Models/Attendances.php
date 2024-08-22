fcb698542ea66ad783761ec110c4793f75d5f1eb
promisepioneer <fifirman000@gmail.com>
2024-08-22 19:47:15 +0700

    updating attendancesSUmmary

--- app/Models/Attendances.php
+++ app/Models/Attendances.php
@@ -50,7 +50,7 @@ class Attendances extends Model
     {
         return $groupedData->map(function ($items) {
             $checkIn = $items->where('status1', 0)->first();
-            $checkOut = $items->where('status1', 1)->last();
+            $checkOut = $items->where('status1', 1)->first();
 
             $userWorktime = WorkTime::where('name', $checkIn?->work_time)->first();
             $defaultWorkTime = WorkTime::where('id', 1)->first();
