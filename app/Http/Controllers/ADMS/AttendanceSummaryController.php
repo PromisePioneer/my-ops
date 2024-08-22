3df0cab386f6503d2de144a7597dc9e5458474cf
promisepioneer <fifirman000@gmail.com>
2024-08-22 18:38:18 +0700

    updating attendancesSUmmary

--- app/Http/Controllers/ADMS/AttendanceSummaryController.php
+++ app/Http/Controllers/ADMS/AttendanceSummaryController.php
@@ -83,16 +83,15 @@ class AttendanceSummaryController extends Controller
         ]);
     }
 
-
     public function attendanceSummaryDetailForOneMonthBasedOnUserId($month, $year, $employeeId): JsonResponse
     {
-        $attendances = $this->attendances->attendanceSummaryDetailForOneMonthBasedOnUserId($month, $year,
-            $employeeId, $this->perPage);
+        // Ambil dan group data absensi berdasarkan employee_id dan tanggal
+        $attendances = $this->attendanceSummaryDetailForOneMonthBasedOnUserId($month, $year, $employeeId,
+            $this->perPage);
 
-
+        // Ambil data untuk total kehadiran dan total keterlambatan dalam satu bulan
         $totalPresentAndTotalMinutesLate = Attendances::select('attendances.employee_id', 'users.name as user_name',
-            'attendances.timestamp',
-            'attendances.status1', 'work_time.name as work_time', 'users.nip as user_nip')
+            'attendances.timestamp', 'attendances.status1', 'work_time.name as work_time', 'users.nip as user_nip')
             ->join('users', 'users.absent_id', '=', 'attendances.employee_id')
             ->leftJoin('user_work_time', 'user_work_time.user_id', '=', 'users.id')
             ->leftJoin('work_time', 'work_time.id', '=', 'user_work_time.work_time_id')
@@ -107,29 +106,33 @@ class AttendanceSummaryController extends Controller
         $summaryData = $totalPresentAndTotalMinutesLate->map(function ($items) {
             $totalMinutesLate = 0;
             $totalPresent = $items->where('status1', 0)->count();
-            // Group by each day to calculate daily lateness
+
+            // Group by setiap hari untuk menghitung keterlambatan harian
             $dailyAttendances = $items->groupBy(function ($item) {
                 return Carbon::parse($item->timestamp)->format('Y-m-d');
             });
 
-
             foreach ($dailyAttendances as $day => $dailyItems) {
                 $checkIn = $dailyItems->where('status1', 0)->first();
-                $userWorktime = WorkTime::where('name', $checkIn?->work_time)->first();
-                $defaultWorkTime = WorkTime::where('id', 1)->first();
-                $expectedCheckInTime = $userWorktime ? $userWorktime->clock_in : $defaultWorkTime->clock_in;
 
-                // Combine the date of check-in with the expected time
-                $expectedCheckIn = Carbon::parse($checkIn?->timestamp)->format('Y-m-d').' '.$expectedCheckInTime;
-                $expectedCheckIn = Carbon::parse($expectedCheckIn);
+                if ($checkIn) { // Pastikan ada data check-in
+                    $userWorktime = WorkTime::where('name', $checkIn->work_time)->first();
+                    $defaultWorkTime = WorkTime::where('id', 1)->first();
+                    $expectedCheckInTime = $userWorktime ? $userWorktime->clock_in : $defaultWorkTime->clock_in;
 
-                // Calculate lateness in minutes for that day
-                $actualCheckIn = Carbon::parse($checkIn?->timestamp);
-                if ($actualCheckIn->greaterThan($expectedCheckIn)) {
-                    $minutesLate = $expectedCheckIn->diffInMinutes($actualCheckIn);
-                    $totalMinutesLate += $minutesLate;
+                    // Gabungkan tanggal check-in dengan waktu yang diharapkan
+                    $expectedCheckIn = Carbon::parse($checkIn->timestamp)->format('Y-m-d').' '.$expectedCheckInTime;
+                    $expectedCheckIn = Carbon::parse($expectedCheckIn);
+
+                    // Hitung keterlambatan dalam menit untuk hari tersebut
+                    $actualCheckIn = Carbon::parse($checkIn->timestamp);
+                    if ($actualCheckIn->greaterThan($expectedCheckIn)) {
+                        $minutesLate = $expectedCheckIn->diffInMinutes($actualCheckIn);
+                        $totalMinutesLate += $minutesLate;
+                    }
                 }
             }
+
             return [
                 'totalMinutesLate' => (int) $totalMinutesLate.' Menit',
                 'total_present' => $totalPresent.' Hari',
@@ -138,10 +141,9 @@ class AttendanceSummaryController extends Controller
             ];
         })->values();
 
-
         return response()->json([
             'data' => $attendances,
-            'summary_data' => $summaryData[0]
+            'summary_data' => $summaryData->first() // Mengambil data pertama dari collection
         ]);
     }
 }
