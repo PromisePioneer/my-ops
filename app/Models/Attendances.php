bc801be81323e1636ded8e03e5a641da88745a3e
promisepioneer <fifirman000@gmail.com>
2024-08-24 06:20:42 +0700

    updating attendancesSUmmary

--- app/Models/Attendances.php
+++ app/Models/Attendances.php
@@ -34,8 +34,7 @@ class Attendances extends Model
         )
             ->join('users', 'users.absent_id', '=', 'attendances.employee_id')
             ->join('user_work_time', 'user_work_time.user_id', '=', 'users.id')
-            ->leftJoin('work_time', 'work_time.id', '=', 'user_work_time.work_time_id')
-            ->get();
+            ->leftJoin('work_time', 'work_time.id', '=', 'user_work_time.work_time_id');
 
         $paginator = $query->paginate($perPage);
 
