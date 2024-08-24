3f6c6c410272b04cc8ff9c7f9999c798455d5538
promisepioneer <fifirman000@gmail.com>
2024-08-24 14:08:54 +0700

    updating attendancesSUmmary

--- resources/views/pages/adms/attendances-summary/detail.blade.php
+++ resources/views/pages/adms/attendances-summary/detail.blade.php
@@ -209,7 +209,6 @@
                 async getAttendanceSummaryDetailForEachUser() {
                     const resp = await axios.get(`/adms/attendances-summary/detail/data/user/detail/${this.month}/${this.year}/${this.userId}`);
                     this.usersDetail = resp.data;
-                    console.log(usersDetail?.total_present);
                 },
 
                 async nextPageForUserSummary() {
