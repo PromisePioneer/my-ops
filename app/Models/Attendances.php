7d1e17f9b56b0d41bfb44374bd05631d308da286
promisepioneer <fifirman000@gmail.com>
2024-08-24 09:09:20 +0700

    updating attendancesSUmmary

--- app/Models/Attendances.php
+++ app/Models/Attendances.php
@@ -301,9 +301,6 @@ class Attendances extends Model
             $checkIn = $items->where('status1', 0)->first();
             $checkOut = $items->where('status1', 1)->first();
 
-            if ($checkIn) {
-                return null;
-            }
 
             if ($checkIn) {
                 $userWorktime = WorkTime::where('name', $checkIn?->work_time)->first();
