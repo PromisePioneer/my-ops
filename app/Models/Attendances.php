f4c62ff09aa3aeb8da5f7e477e3c2bdca995c085
promisepioneer <fifirman000@gmail.com>
2024-08-24 09:13:37 +0700

    updating attendancesSUmmary

--- app/Models/Attendances.php
+++ app/Models/Attendances.php
@@ -297,8 +297,6 @@ class Attendances extends Model
 
     private function formatGroupedData($groupedData)
     {
-        dd($groupedData);
-
         return $groupedData->map(function ($items) {
             $checkIn = $items->where('status1', 0)->first();
             $checkOut = $items->where('status1', 1)->first();
