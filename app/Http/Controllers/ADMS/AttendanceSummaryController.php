0624d9047017e38cf87cbb7918f3b16bfa68c685
promisepioneer <fifirman000@gmail.com>
2024-08-25 14:33:13 +0700

    updating AttendancesSummary

--- app/Http/Controllers/ADMS/AttendanceSummaryController.php
+++ app/Http/Controllers/ADMS/AttendanceSummaryController.php
@@ -89,22 +89,18 @@ class AttendanceSummaryController extends Controller
         $attendances = $this->attendances->attendanceSummaryDetailForOneMonthBasedOnUserId($month, $year,
             $employeeId, 10);
 
-//        $totalPresentAndTotalMinutesLate = Attendances::select('attendances.employee_id', 'users.name as user_name',
-//            'attendances.timestamp', 'attendances.status1', 'work_time.name as work_time', 'users.nip as user_nip')
-//            ->join('users', 'users.absent_id', '=', 'attendances.employee_id')
-//            ->where('users.absent_id', $employeeId)
-//            ->leftJoin('user_work_time', 'user_work_time.user_id', '=', 'users.id')
-//            ->leftJoin('work_time', 'work_time.id', '=', 'user_work_time.work_time_id')
-//            ->whereMonth('attendances.timestamp', $month)
-//            ->whereYear('attendances.timestamp', $year)
-//            ->get()
-//            ->groupBy(function ($item) {
-//                return $item->user_name;
-//            });
-
-
-        $totalPresentAndTotalMinutesLate = $this->attendances->formatGroupedDataForAttendancesSummary($attendances,
-            $month, $year);
+        $totalPresentAndTotalMinutesLate = Attendances::select('attendances.employee_id', 'users.name as user_name',
+            'attendances.timestamp', 'attendances.status1', 'work_time.name as work_time', 'users.nip as user_nip')
+            ->join('users', 'users.absent_id', '=', 'attendances.employee_id')
+            ->where('users.absent_id', $employeeId)
+            ->leftJoin('user_work_time', 'user_work_time.user_id', '=', 'users.id')
+            ->leftJoin('work_time', 'work_time.id', '=', 'user_work_time.work_time_id')
+            ->whereMonth('attendances.timestamp', $month)
+            ->whereYear('attendances.timestamp', $year)
+            ->get()
+            ->groupBy(function ($item) {
+                return $item->user_name;
+            });
 
 
         $summaryData = $totalPresentAndTotalMinutesLate->map(function ($items) {
