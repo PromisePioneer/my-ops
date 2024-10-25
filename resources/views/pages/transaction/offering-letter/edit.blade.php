8262d17de695af60c777a1f8aae1a0f003e2c3b0
promisepioneer <fifirman000@gmail.com>
2024-10-25 11:06:22 +0700

    Update

--- resources/views/pages/transaction/offering-letter/edit.blade.php
+++ resources/views/pages/transaction/offering-letter/edit.blade.php
@@ -15,7 +15,7 @@
         </style>
     @endpush
     <div class="d-flex flex-column flex-lg-row" x-data="generateOfferingLetter">
-        @include('pages.master.contact.modal.create')
+        @include('pages.general-master-data.contact.modal.create')
 
         <div class="flex-lg-row-fluid mb-10 mb-lg-0 me-lg-7 me-xl-10">
             <div class="card p-5">
