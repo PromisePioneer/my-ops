1538214010ee5e71a8752e0a2e024adbcfbfdd44
promisepioneer <fifirman000@gmail.com>
2024-10-25 11:05:39 +0700

    Update

--- resources/views/pages/transaction/bast/edit.blade.php
+++ resources/views/pages/transaction/bast/edit.blade.php
@@ -9,7 +9,7 @@
         </style>
     @endpush
     <div class="d-flex flex-column flex-lg-row" x-data="generateBAST">
-        @include('pages.master.contact.modal.create')
+        @include('pages.general-master-data.contact.modal.create')
         <div class="flex-lg-row-fluid mb-10 mb-lg-0 me-lg-7 me-xl-10">
             <div class="card">
                 <div class="card-body p-12">
