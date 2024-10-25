3a39a3cc33950dcaae5fa54feaba6b24351547c7
promisepioneer <fifirman000@gmail.com>
2024-10-25 14:46:05 +0700

    #75 Done.

--- resources/views/pages/general-master-data/contact/modal/create.blade.php
+++ resources/views/pages/general-master-data/contact/modal/create.blade.php
@@ -18,10 +18,10 @@
                 <form id="contactFormCreate" @submit.prevent="saveContact()">
                     <div class="d-flex flex-column mb-8 fv-row">
                         <label class="d-flex align-items-center fs-6 fw-bold mb-2">
-                            <span class="required">Nama Lengkap</span>
+                            <span class="required">Nama PIC</span>
                         </label>
                         <input type="text" class="form-control form-control-solid" placeholder="Nama Lengkap"
-                               name="full_name"/>
+                               name="pic_name"/>
                     </div>
                     <div class="row g-9 mb-8">
                         <div class="col-md-6 fv-row">
@@ -31,7 +31,7 @@
                         </div>
 
                         <div class="col-md-6 fv-row">
-                            <label class="required fs-6 fw-bold mb-2">Email</label>
+                            <label class="fs-6 fw-bold mb-2">Email</label>
                             <input type="text" class="form-control form-control-solid" placeholder="Email"
                                    name="email"/>
                         </div>
@@ -44,7 +44,7 @@
                     <div class="row mb-4">
                         <div class="col-md-2">
                             <label class="d-flex align-items-center fs-6 fw-bold mb-2">
-                                <span class="required">Identitas</span>
+                                <span>Identitas</span>
                             </label>
                             <select name="identity_type" class="form-select form-select-solid"
                                     data-placeholder="Select an option">
@@ -55,7 +55,7 @@
                         </div>
                         <div class="col-md-10">
                             <label class="d-flex align-items-center fs-6 fw-bold mb-2">
-                                <span class="required">No. Identitas</span>
+                                <span>No. Identitas</span>
                             </label>
                             <input type="text" class="form-control form-control-solid" placeholder="No. Identitas"
                                    name="identity_number"/>
@@ -64,22 +64,22 @@
                     <div class="row mb-4">
                         <div class="col-md-6">
                             <label class="d-flex align-items-center fs-6 fw-bold mb-2">
-                                <span class="required">FAX</span>
+                                <span>FAX</span>
                             </label>
                             <input type="text" class="form-control form-control-solid" placeholder="FAX" name="fax"/>
                         </div>
                         <div class="col-md-6">
                             <label class="d-flex align-items-center fs-6 fw-bold mb-2">
-                                <span class="required">NPWP</span>
+                                <span>NPWP</span>
                             </label>
-                            <input type="text" class="form-control form-control-solid" placeholder="No. Identitas"
+                            <input type="text" class="form-control form-control-solid" placeholder="NPWP"
                                    name="npwp"/>
                         </div>
                     </div>
                     <div class="row mb-4">
                         <div class="col-md-12">
                             <label class="d-flex align-items-center fs-6 fw-bold mb-2">
-                                <span class="required">Alamat Lengkap</span>
+                                <span>Alamat</span>
                             </label>
                             <textarea name="complete_address" class="form-control form-control-solid" id=""
                                       data-kt-autosize="true"></textarea>
@@ -88,7 +88,7 @@
                     <div class="row mb-10">
                         <div class="col-md-12">
                             <label class="d-flex align-items-center fs-6 fw-bold mb-2">
-                                <span class="required">Other Info</span>
+                                <span>Info Lainnya</span>
                             </label>
                             <textarea name="other_info" class="form-control form-control-solid" id=""
                                       data-kt-autosize="true"></textarea>
