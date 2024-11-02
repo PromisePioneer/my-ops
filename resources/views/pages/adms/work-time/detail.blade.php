378451245e894b21ea93ea825ab0b4c66e8689ce
PromisePioneer <fifirman000@gmail.com>
2024-11-02 11:53:03 +0700

    Update

--- resources/views/pages/adms/work-time/detail.blade.php
+++ resources/views/pages/adms/work-time/detail.blade.php
@@ -145,7 +145,7 @@
                 async paginationEndPoint(url) {
                     if (url) {
                         const resp = await axios.get(`${url}`);
-                        this.branches = resp.data
+                        this.users = resp.data
                     }
                 },
                 toggleAllCheckBox() {
