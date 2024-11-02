28c6af5f7ad60c5e3ec3dc68a24be66929fba449
PromisePioneer <fifirman000@gmail.com>
2024-11-03 01:09:59 +0700

    Update

--- resources/views/pages/general-master-data/department/index.blade.php
+++ resources/views/pages/general-master-data/department/index.blade.php
@@ -187,7 +187,7 @@
                 async paginationEndPoint(url) {
                     if (url) {
                         const resp = await axios.get(`${url}`);
-                        this.branches = resp.data
+                        this.departments = resp.data
                     }
                 },
                 async save() {
