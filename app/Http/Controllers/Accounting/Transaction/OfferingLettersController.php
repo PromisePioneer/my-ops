2739aebfce2a9d241f27d81f9fa40954333901ff
PromisePioneer <fifirman000@gmail.com>
2024-11-12 23:49:25 +0700

    update on fab print preview and fab print

--- app/Http/Controllers/Accounting/Transaction/OfferingLettersController.php
+++ app/Http/Controllers/Accounting/Transaction/OfferingLettersController.php
@@ -277,7 +277,7 @@ class OfferingLettersController extends Controller
             'offeringLetterCompanyName'));
 
         $pdf = Browsershot::html($view)
-            ->setOption('args', ['--no-sandbox'])
+            ->setChromePath('/snap/bin/chromium-browser')
             ->setIncludePath(config('services.browsershot.include_path'))->pdf();
 
 
