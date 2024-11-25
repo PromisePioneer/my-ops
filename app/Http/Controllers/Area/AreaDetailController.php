861ed4c780b621d2fde8f162c6d7f863b0cb487b
PromisePioneer <fifirman000@gmail.com>
2024-11-25 18:44:55 +0700

    update

--- app/Http/Controllers/Area/AreaDetailController.php
+++ app/Http/Controllers/Area/AreaDetailController.php
@@ -41,8 +41,7 @@ class AreaDetailController extends Controller
     {
         $search = $request->search;
 
-        $query = User::whereDoesntHave('userHasArea')->where('active', '=', 1)
-            ->where('branch_id', $area->branch_id)
+        $query = User::whereDoesntHave('userHasArea')
             ->whereHas('roles', function ($query) {
                 $query->whereIn('name', ['Head Engineer', 'Engineer']);
             })
