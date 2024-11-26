a55a7b7840752ae5914cbaa88116c1b0c0e08e97
PromisePioneer <fifirman000@gmail.com>
2024-11-26 09:51:42 +0700

    update

--- resources/views/pages/general-master-data/area/modal/edit.blade.php
+++ resources/views/pages/general-master-data/area/modal/edit.blade.php
@@ -19,6 +19,7 @@
                     <div class="mb-10">
                         <label for="name" class="required form-label">Cabang</label>
                         <select name="branch_id" id="selected-branch"
+                                data-parent-dropdown="#modal-edit"
                                 class="form-select form-select-solid branch-select2">
                             <option></option>
                         </select>
