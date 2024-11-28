7e28687d9076d064557f4af8598033912edd0573
PromisePioneer <fifirman000@gmail.com>
2024-11-28 11:37:25 +0700

    update

--- app/Http/Controllers/HRIS/Attendances/EmployeeScheduleService.php
+++ app/Http/Controllers/HRIS/Attendances/EmployeeScheduleService.php
@@ -54,7 +54,7 @@ class EmployeeScheduleService
 
         if ($request->user()->hasAnyRole('Customer Service Leader')) {
             $user->whereHas('roles', function ($query) use ($request) {
-                $query->whereIn('name', ['Customer Service Leader', 'Customer Service Staff']);
+                $query->whereIn('name', ['Customer Service Leader', 'Customer Service Staff', 'After Sales Customer Service']);
             })->where(function ($query) use ($request) {
                 $query->whereNull('branch_id')->orWhereIn('branch_id', [1])
                     ->where('active', 1);;
@@ -132,7 +132,7 @@ class EmployeeScheduleService
 
         if ($request->user()->hasAnyRole('Customer Service Leader')) {
             $user->whereHas('roles', function ($query) use ($request) {
-                $query->whereIn('name', ['Customer Service Leader', 'Customer Service Staff']);
+                $query->whereIn('name', ['Customer Service Leader', 'Customer Service Staff', 'After Sales Customer Service']);
             })->where(function ($query) use ($request) {
                 $query->whereNull('branch_id')->orWhereIn('branch_id', [1])
                     ->where('active', 1);;
