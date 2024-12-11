c6ca7b38bd8312fcc75d782dd5783d7523c52b07
PromisePioneer <fifirman000@gmail.com>
2024-12-12 02:47:33 +0700

    update

--- app/Service/Attendances/AttendanceSummaryDetailService.php
+++ app/Service/Attendances/AttendanceSummaryDetailService.php
@@ -198,7 +198,7 @@ class AttendanceSummaryDetailService
 
 
             if ($expectedCheckIn->lessThan($actualCheckIn) && $expectedCheckIn->toTimeString() === "00:00:00") {
-                $expectedCheckIn->addDays(2);
+                $expectedCheckIn->addDays();
             }
 
             if ($actualCheckIn->greaterThan($expectedCheckIn)) {
