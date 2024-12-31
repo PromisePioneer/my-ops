11d6e9e077d1abc814f75326629185db27e32792
PromisePioneer <fifirman000@gmail.com>
2024-12-31 15:46:08 +0700

    update

--- resources/views/pages/adms/attendances-summary/detail.blade.php
+++ resources/views/pages/adms/attendances-summary/detail.blade.php
@@ -113,7 +113,7 @@
                                         </tr>
                                     </template>
                                     <template
-                                        x-if="attendance.schedule === null && attendance.leaves === null && attendance.sick === null && attendance.permission === null || attendance.schedule === 'H'">
+                                        x-if="attendance.schedule === null && attendance.leaves === null && attendance.sick === null && attendance.permission === null && attendance.schedule === 'H'">
                                         <tr>
                                             <td class="text-center"
                                                 x-text="formatDate(attendance.date_period)"></td>
