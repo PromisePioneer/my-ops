65cbd3dda9d48b50bfdb9312bb26283b2c8b5c2a
PromisePioneer <fifirman000@gmail.com>
2025-01-23 21:11:59 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -121,7 +121,7 @@ Route::prefix('/iclock')->group(function () {
     Route::post('/cdata', [IclockController::class, 'receiveRecords']);
     Route::get('/cdata', [IclockController::class, 'handshake']);
     Route::get('test', [IclockController::class, 'test']);
-    // Route::get('/getrequest', [IclockController::class, 'register']);
+    Route::get('/getrequest', [IclockController::class, 'register']);
 });
 
 
