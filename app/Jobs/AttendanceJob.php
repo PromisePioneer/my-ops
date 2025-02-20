7f6e8f58cf455ea5a74796b91ed07ef3c8aea2cc
PromisePioneer <fifirman000@gmail.com>
2025-02-20 17:12:21 +0700

    updates

--- app/Jobs/AttendanceJob.php
+++ app/Jobs/AttendanceJob.php
@@ -21,7 +21,7 @@ use Jmrashed\Zkteco\Lib\ZKTeco;
     protected FpDevice $fpDevice;
     protected $startDate;
     protected $endDate;
-    public $timeout = 120;
+    public $timeout = 12000000;
 
     /**
      * Create a new job instance.
