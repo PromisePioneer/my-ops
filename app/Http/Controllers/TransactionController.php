7984d275c472b29faad5b36f553f06576e0209f6
PromisePioneer <fifirman000@gmail.com>
2025-03-08 11:14:11 +0700

    updates

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -5,8 +5,8 @@ namespace App\Http\Controllers;
 use AllowDynamicProperties;
 use App\Http\Requests\TransactionRequest;
 use App\Models\Transaction;
-use App\Models\UnitType;
 use App\Service\Transactions\TransactionService;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
@@ -14,7 +14,6 @@ use Throwable;
 
 #[AllowDynamicProperties] class TransactionController extends Controller
 {
-    private static int $perPage = 10;
 
 
     public function __construct()
@@ -22,23 +21,39 @@ use Throwable;
         $this->transactionService = new TransactionService();
     }
 
+    /**
+     * @throws AuthorizationException
+     */
     public function index(): View
     {
+        $this->authorize('view', Transaction::class);
         return view('pages.transactions.index');
     }
 
+    /**
+     * @throws AuthorizationException
+     */
     public function data(): JsonResponse
     {
+        $this->authorize('view', Transaction::class);
         return response()->json($this->transactionService->data());
     }
 
+    /**
+     * @throws AuthorizationException
+     */
     public function filter(Request $request): JsonResponse
     {
+        $this->authorize('view', Transaction::class);
         return response()->json($this->transactionService->filter($request));
     }
 
+    /**
+     * @throws AuthorizationException
+     */
     public function search(Request $request): JsonResponse
     {
+        $this->authorize('view', Transaction::class);
         return response()->json($this->transactionService->search($request));
     }
 
@@ -47,18 +62,27 @@ use Throwable;
      */
     public function store(TransactionRequest $request): JsonResponse
     {
+        $this->authorize('create', Transaction::class);
         $this->transactionService->store($request);
         return response()->json(['message' => 'Data berhasil disimpan.']);
     }
 
 
+    /**
+     * @throws AuthorizationException
+     */
     public function edit(Transaction $transaction): JsonResponse
     {
+        $this->authorize('edit', Transaction::class);
         return response()->json($transaction);
     }
 
+    /**
+     * @throws AuthorizationException
+     */
     public function update(TransactionRequest $request, Transaction $transaction): JsonResponse
     {
+        $this->authorize('update', Transaction::class);
         $this->transactionService->update($request, $transaction);
         return response()->json([
             'message' => 'Data berhasil disimpan.'
@@ -71,13 +95,20 @@ use Throwable;
      */
     public function confirm(Transaction $transaction): JsonResponse
     {
+        $this->authorize('confirm', $transaction);
         $this->transactionService->confirm($transaction);
-        return response()->json();
+        return response()->json([
+            'message' => 'data berhasil disimpan'
+        ]);
     }
 
 
+    /**
+     * @throws AuthorizationException
+     */
     public function destroy(Request $request, Transaction $transaction): JsonResponse
     {
+        $this->authorize('delete', $transaction);
         $implodeID = implode(',', $request->get('id'));
         $explodeID = explode(',', $implodeID);
         $transaction->whereIn('id', $explodeID)->delete();
@@ -85,17 +116,5 @@ use Throwable;
         return response()->json([
             'message' => 'data berhasil dihapus'
         ]);
-    }
-
-
-    public function getUnitType(Request $request, UnitType $unitType): JsonResponse
-    {
-        return response()->json($unitType->getData($request));
-    }
-
-
-    public function selectedUnitType(UnitType $unitType): JsonResponse
-    {
-        return response()->json($this->selectedUnitType($unitType->id));
     }
 }
