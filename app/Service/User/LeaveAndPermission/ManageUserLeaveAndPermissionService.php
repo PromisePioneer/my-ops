569f5480d22b84aba5cff2010c7d5c162f3c7f25
PromisePioneer <fifirman000@gmail.com>
2025-03-11 11:17:19 +0700

    fix user list on leaves user select2

--- app/Service/User/LeaveAndPermission/ManageUserLeaveAndPermissionService.php
+++ app/Service/User/LeaveAndPermission/ManageUserLeaveAndPermissionService.php
@@ -75,9 +75,7 @@ use function App\Helper\formatDate;
     public function getUserData(Request $request)
     {
         $search = $request->search;
-        $query = User::with('userHasArea', 'branch', 'roles')
-            ->orderBy('name')
-            ->select('id', 'name', 'nip');
+        $query = User::with('userHasArea', 'branch', 'roles');
 
 
         if (!empty($search)) {
