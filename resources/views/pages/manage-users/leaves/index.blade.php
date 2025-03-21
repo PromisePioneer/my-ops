2cf1bb9144855f75ba64d411ea65a9df5ce6fec0
PromisePioneer <fifirman000@gmail.com>
2025-03-21 20:05:44 +0700

    update

--- resources/views/pages/manage-users/leaves/index.blade.php
+++ resources/views/pages/manage-users/leaves/index.blade.php
@@ -186,7 +186,7 @@
                                             <template x-if="leave.confirmation_status === 'Diproses'">
                                                 <td>
                                                     <template
-                                                        x-if="Number(confirmPermission) === 1 || Number(userSessionId) !== leave.user_id">
+                                                        x-if="Number(confirmPermission) === 1 && Number(userSessionId) !== leave.user_id">
                                                         <button class="btn btn-info btn-sm" data-bs-toggle="modal"
                                                                 data-bs-target="#modal-confirm"
                                                                 @click="edit(leave.id)">
