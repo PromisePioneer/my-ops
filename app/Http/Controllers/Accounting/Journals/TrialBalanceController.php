57ee24fa254d72581c882e72bcfd73d42e8ccfa1
PromisePioneer <fifirman000@gmail.com>
2025-04-05 11:20:45 +0700

    update

--- app/Http/Controllers/Accounting/Journals/TrialBalanceController.php
+++ app/Http/Controllers/Accounting/Journals/TrialBalanceController.php
@@ -42,7 +42,7 @@ class TrialBalanceController extends Controller
 
         return response()->json([
             'trial_balances' => $this->trialBalanceService->data(),
-            'total_debit' => 'Rp.' . number_format($totalDebit - $totalCredit, 2, '.', '.'),
+            'total_debit' => 'Rp.' . number_format($totalDebit, 2, '.', '.'),
             'total_credit' => 'Rp.' . number_format($totalCredit, 2, '.', '.'),
         ]);
     }
