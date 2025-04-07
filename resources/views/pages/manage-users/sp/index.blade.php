d7245277049864e292550900bd8c2031f167a139
PromisePioneer <fifirman000@gmail.com>
2025-04-07 10:49:29 +0700

    update

--- resources/views/pages/manage-users/sp/index.blade.php
+++ resources/views/pages/manage-users/sp/index.blade.php
@@ -109,8 +109,9 @@
                                                                       width="8.15546" height="2" rx="1"
                                                                       transform="rotate(45 17.0365 15.1223)"
                                                                       fill="black"></rect>
-																<path d="M11 19C6.55556 19 3 15.4444 3 11C3 6.55556 6.55556 3 11 3C15.4444 3 19 6.55556 19 11C19 15.4444 15.4444 19 11 19ZM11 5C7.53333 5 5 7.53333 5 11C5 14.4667 7.53333 17 11 17C14.4667 17 17 14.4667 17 11C17 7.53333 14.4667 5 11 5Z"
-                                                                      fill="black"></path>
+																<path
+                                                                    d="M11 19C6.55556 19 3 15.4444 3 11C3 6.55556 6.55556 3 11 3C15.4444 3 19 6.55556 19 11C19 15.4444 15.4444 19 11 19ZM11 5C7.53333 5 5 7.53333 5 11C5 14.4667 7.53333 17 11 17C14.4667 17 17 14.4667 17 11C17 7.53333 14.4667 5 11 5Z"
+                                                                    fill="black"></path>
 															</svg>
 														</span>
                                 <input type="text" class="form-control form-control-solid w-250px ps-15"
@@ -249,18 +250,22 @@
                     this.spList = resp.data;
                     this.startIndex = this.spList.from;
                 },
-                async nextPage() {
-                    if (this.spList.next_page_url) {
-                        const resp = await axios.get(`${this.spList.next_page_url}`);
-                        this.spList = resp.data
-                        this.startIndex = this.spList.from
-                    }
-                },
-                async previousPage() {
-                    if (this.spList.prev_page_url) {
-                        const resp = await axios.get(`${this.spList.prev_page_url}`);
-                        this.spList = resp.data
-                        this.startIndex = this.spList.from
+                async paginationEndPoint(url) {
+                    if (url) {
+                        this.spList = [];
+                        this.isLoading = true;
+                        try {
+                            const resp = await axios.get(`${url}`, {
+                                params: {
+                                    search: this.search,
+                                }
+                            });
+                            this.spList = resp.data
+                        } catch (e) {
+                            console.log(e)
+                        } finally {
+                            this.isLoading = false
+                        }
                     }
                 },
                 async getMainBranches() {
