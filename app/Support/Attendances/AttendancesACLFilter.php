ca1cea4c2b6b2dd6aff2b7f4b3fde4ad2193e272
PromisePioneer <fifirman000@gmail.com>
2025-04-09 16:01:54 +0700

    update

--- app/Support/Attendances/AttendancesACLFilter.php
+++ app/Support/Attendances/AttendancesACLFilter.php
@@ -55,7 +55,7 @@ class AttendancesACLFilter
 
             $query->whereHas('roles', function ($query) use ($request) {
                 $query->whereIn('name', ['Mechanic Senior Staff', 'Mechanic Helper Staff']);
-            })->whereNull('branch_id');
+            });
         }
 
 
