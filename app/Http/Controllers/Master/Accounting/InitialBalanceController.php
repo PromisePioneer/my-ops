04c9972dd60684329755176d043238bb1166f3c4
PromisePioneer <fifirman000@gmail.com>
2025-04-11 16:02:21 +0700

    update

--- app/Http/Controllers/Master/Accounting/InitialBalanceController.php
+++ app/Http/Controllers/Master/Accounting/InitialBalanceController.php
@@ -123,8 +123,6 @@ use Illuminate\Http\Request;
         $this->authorize('create', AccountTransaction::class);
         $rawAmount = $request->input('amount');
 
-
-        dd($rawAmount);
         $formattedValue = str_replace(',', '.', str_replace('.', '', $rawAmount));
         $amount = number_format((float)$formattedValue, 4, '.', '');
 
