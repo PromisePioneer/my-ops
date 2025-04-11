5766b5e1050f9073acf124698bdf8cf07ff7d69f
PromisePioneer <fifirman000@gmail.com>
2025-04-11 15:56:05 +0700

    update

--- app/Http/Controllers/Master/Accounting/InitialBalanceController.php
+++ app/Http/Controllers/Master/Accounting/InitialBalanceController.php
@@ -133,7 +133,7 @@ use Illuminate\Http\Request;
         ], [
             'date' => Carbon::now()->subYear()->endOfYear(),
             'transaction_type' => 'SA',
-            'amount' => (float)$amount ?? 0,
+            'amount' => (float)$amount,
         ]);
 
         return response()->json(['message' => 'Saldo awal berhasil ditambahkan.']);
