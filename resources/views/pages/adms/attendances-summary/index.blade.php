d72c8055feffc6fdef8383290f3cc5fa45c2306a
PromisePioneer <fifirman000@gmail.com>
2025-04-26 13:18:02 +0700

    update

--- resources/views/pages/adms/attendances-summary/index.blade.php
+++ resources/views/pages/adms/attendances-summary/index.blade.php
@@ -169,12 +169,6 @@
                                                             x-text="`${attendance.total_sick}`"></td>
                                                     </tr>
                                                     <tr class="bg-gray-100 text-center">
-                                                        <td class="min-w-125px">Alfa</td>
-                                                        <td class="min-w-125px">:</td>
-                                                        <td class="min-w-125px"
-                                                            x-text="`${attendance.total_absent}`"></td>
-                                                    </tr>
-                                                    <tr class="bg-gray-100 text-center">
                                                         <td class="min-w-125px">Cuti Penting</td>
                                                         <td class="min-w-125px">:</td>
                                                         <td class="min-w-125px"
