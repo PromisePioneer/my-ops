a4746afbf5d65fb81da3f85dd02fc84e170a9b4d
PromisePioneer <fifirman000@gmail.com>
2025-06-03 11:55:37 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -482,6 +482,7 @@ Route::group(['middleware' => ['auth']], static function () {
             Route::prefix('assets')->group(function () {
                 Route::post('/destroy', [AssetController::class, 'destroy']);
                 Route::get('/', [AssetController::class, 'index']);
+                Route::get('/data', [AssetController::class, 'data']);
                 Route::get('/create', [AssetController::class, 'create']);
                 Route::get('/{asset}', [AssetController::class, 'edit']);
                 Route::get('/data', [AssetController::class, 'data']);
