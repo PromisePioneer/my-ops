2b6e91576046b9ffbae28710ad1a87ea1328d047
PromisePioneer <fifirman000@gmail.com>
2025-06-23 03:57:42 +0000

    update

--- resources/views/layouts/partials/aside.blade.php
+++ resources/views/layouts/partials/aside.blade.php
@@ -295,7 +295,7 @@
                         @endslot
                     </x-dropdown-menu>
                 @endcanany
-                <x-menu-sections>Inventory</x-menu-sections>
+                {{-- <x-menu-sections>Inventory</x-menu-sections>
                 <x-dropdown-menu :active="request()->is('inventory/*')">
                     @slot('parentIcon')
                         <i class="ki-duotone ki-dollar fs-2">
@@ -329,7 +329,7 @@
                             Mutasi Barang
                         </x-dropdown-menu-item>
                     @endslot
-                </x-dropdown-menu>
+                </x-dropdown-menu> --}}
 
 
                 @canany(['Lihat Menu Jurnal Umum', 'Lihat Menu Buku Besar', 'Lihat Menu Neraca Saldo'])
