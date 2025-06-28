d05e3382ca4af8ea0588df088fe09e019349624b
PromisePioneer <fifirman000@gmail.com>
2025-06-28 09:39:03 +0700

    revision on leaves
    # Conflicts:
    #	app/Http/Controllers/HRIS/Correspondence/LeaveAndPermissionController.php
    #	resources/views/pages/manage-users/leaves/index.blade.php
    #	resources/views/pages/utilities/user-profile/leaves/index.blade.php

--- resources/views/pages/manage-users/leaves/index.blade.php
+++ resources/views/pages/manage-users/leaves/index.blade.php
@@ -24,7 +24,6 @@
                                         data-bs-toggle="modal"
                                         data-bs-target="#modal-leaves"
                                 >
-                                    <x-icons.add-item/>
                                     Tambah
                                 </button>
                             </div>
@@ -133,7 +132,7 @@
                                                     <span class="text-danger">Tanggal akan ditentukan jika surat resmi terbukti benar dan sesuai.</span>
                                                 </template>
                                                 <template
-                                                    x-if="leave.start_date === null && leave.end_date === null && leave.important_leaves === 'Mendapat Musibah'  && leave.confirmation_status === 'Diproses'">
+                                                    x-if="leave.start_date === null && leave.end_date === null && leave.important_leaves === 'Musibah'  && leave.confirmation_status === 'Diproses'">
                                                     <span class="text-danger">
                                                         Tanggal akan ditetapkan sesuai dengan pertimbangan perusahaan.
                                                     </span>
