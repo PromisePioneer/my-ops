d4a23c8d98ff2705d3c4466823524f61c1fcb0f2
PromisePioneer <fifirman000@gmail.com>
2025-06-28 14:58:43 +0700

    update

--- resources/views/layouts/partials/aside.blade.php
+++ resources/views/layouts/partials/aside.blade.php
@@ -283,6 +283,7 @@
                         @endslot
                     </x-dropdown-menu>
                 @endcanany
+                @can('Lihat Menu Inventory')
                 <x-menu-sections>Inventory</x-menu-sections>
                 <x-dropdown-menu :active="request()->is('inventory/*')">
                     @slot('parentIcon')
@@ -318,6 +319,7 @@
                         </x-dropdown-menu-item>
                     @endslot
                 </x-dropdown-menu>
+                @endcan
 
 
                 @canany(['Lihat Menu Jurnal Umum', 'Lihat Menu Buku Besar', 'Lihat Menu Neraca Saldo'])
