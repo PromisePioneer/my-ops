972dae64f328c23f099513b5913a7ba207026573
PromisePioneer <fifirman000@gmail.com>
2025-07-05 14:20:57 +0700

    update

--- app/Models/EmployeeSchedule.php
+++ app/Models/EmployeeSchedule.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use App\Models\Master\Common\Branch;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Spatie\Activitylog\Contracts\Activity;
