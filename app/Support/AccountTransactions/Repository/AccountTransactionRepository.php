c0904e1b1298a8430145b3abcf52e5313a801ae0
PromisePioneer <fifirman000@gmail.com>
2025-07-14 20:56:11 +0700

    update

--- app/Support/AccountTransactions/Repository/AccountTransactionRepository.php
+++ app/Support/AccountTransactions/Repository/AccountTransactionRepository.php
@@ -3,6 +3,7 @@
 namespace App\Support\AccountTransactions\Repository;
 
 use AllowDynamicProperties;
+use App\Models\AccountingPeriod;
 use App\Models\AccountTransaction;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -17,10 +18,12 @@ use Illuminate\Http\Request;
 
     public function findByType(string $type, Request $request)
     {
-        return AccountTransaction::with(['account.parent', function ($query) use ($type, $request) {
-            $query->where('company_id', $request->session()->get('company_session'))
-                ->where('trial_balance_type', $type);
-        }])->where('transaction_type', 'SA')
+        return AccountTransaction::with('account.parent')
+            ->whereYear('date', AccountingPeriod::first()->year)
+            ->whereHas('account.parent', function ($query) use ($type, $request) {
+                $query->where('company_id', $request->session()->get('company_session'))
+                    ->where('trial_balance_type', $type);
+            })->where('transaction_type', 'SA')
             ->where('entries_type', $type);
     }
 }
